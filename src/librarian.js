e92bbc827332b832b8f70b115244340fac58f1a8
Lourdes Benites <lourdesben95@gmail.com>
2022-02-06 16:42:34 -0700

    Add librarian

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,21 @@
 class Librarian {
-  constructor() {
-    
+  constructor(librarianName, library) {
+    this.name = librarianName;
+    this.library = library;
+  }
+  greetPatron(personName, morning) {
+    if (!morning) {
+      return `Hello, ${personName}!`
+    }
+    return `Good morning, ${personName}!`
+  }
+  findBook(bookName) {
+    for (var i = 0; i < this.library.shelves.fantasy.length; i++) {
+      if (bookName === this.library.shelves.fantasy[i].title) {
+        return `Yes, we have ${bookName}`
+        console.log(checkoutBook)
+      } return `Sorry, we do not have ${bookName}`
+    }
   }
 }
 
