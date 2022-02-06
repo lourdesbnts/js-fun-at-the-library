ee22a0c84c6264929f8a87c9e9d136b37813ce30
Lourdes Benites <lourdesben95@gmail.com>
2022-02-05 19:40:08 -0700

    Add shelf and library test

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,40 @@
+function createLibrary(libraryName) {
+  var library = {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+  return library
+}
 
+function addBook(library, book) {
+  if (book.genre === 'fantasy') {
+    library.shelves.fantasy.push(book)
+  }
+  if (book.genre === 'nonFiction') {
+    library.shelves.nonFiction.push(book)
+  }
+  if (book.genre === 'fiction') {
+    library.shelves.fiction.push(book)
+  }
+  return library
+}
+
+function checkoutBook(library, bookName, genre) {
+  for (var i = 0; i < library.shelves[genre].length; i++) {
+    if (library.shelves[genre][i].title === bookName) {
+      library.shelves[genre].splice(i, 1)
+      return `You have now checked out ${bookName} from the ${library.name}`
+    }
+  }
+  return `Sorry, there are currently no copies of ${bookName} available at the ${library.name}`
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
--- src/shelf.js
+++ src/shelf.js
@@ -4,13 +4,36 @@ function shelfBook(book, array) {
   }
 }
 
-function unshelfBook() {
-  
+function unshelfBook(title, bookShelf) {
+  for (var i = 0; i < bookShelf.length; i++) {
+    if (bookShelf[i].title === title) {
+      bookShelf.splice(i, 1)
+    }
+  }
 }
+
+function listTitles(shelf) {
+  var arr = [];
+  for (var i = 0; i < shelf.length; i++) {
+    arr.push(shelf[i].title);
+  }
+  return arr.join(', ')
+}
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      return true
+    }
+  }
+  return false
+}
+
+
 
 module.exports = {
  shelfBook,
  unshelfBook,
-  // listTitles,
-  // searchShelf
+ listTitles,
+ searchShelf
 };
